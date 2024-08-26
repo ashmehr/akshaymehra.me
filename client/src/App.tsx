c1c97768e0c2f285a90620e180e7dc767b47ace5
Akshay Mehra <akshaymehra@Akshays-MacBook-Pro.local>
2024-08-26 00:28:57 -0400

    Added gradient and animation, reworked layout slightly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,47 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-import loremIpsum from './assets/loremipsum';
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <header>
-      <ul>
-        <li>
-          <a href="#home">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </header>
-
-    <div id="home">
-      <h1>Akshay Mehra</h1>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
+    <div className="BlurCircle"></div>
+    <div id="Home">
+      <h1 className="NameHeader">Akshay Mehra
+        <span id="caret">|</span></h1>
+      <h2 className="SubHeader">Software Engineer | Manager</h2>
     </div>
-    
-    <div id="about">
-      About! {loremIpsum}
+    <div className="ButtonContainer">
+      <div className="Button">
+        <a href="#home">Home</a>
+      </div>
+      <div className="Button">
+        <a href="#about">About</a>
+      </div>
+      <div className="Button">
+        <a href="#projects">Projects</a>
+      </div>
+      <div className="Button">
+        <a href="#contact">Contact</a>
+      </div>
     </div>
-    <div id="projects"> Projects! {loremIpsum}</div>
-    <div id="contact"> Contact! {loremIpsum}</div>
     </>
   )
 }
